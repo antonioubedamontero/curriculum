210f5fed08cb61f7b27286017de5b816d7496c90
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-04-24 12:31:14 +0200

    basic structure and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./pages/curriculum/curriculum.component').then(page => page.CurriculumComponent)
+  },
+  {
+    path: ':lang/web-developer',
+    loadComponent: () => import('./pages/curriculum/curriculum.component').then(page => page.CurriculumComponent)
+  },
+  {
+    path: 'en',
+    loadComponent: () => import('./pages/curriculum/curriculum.component').then(page => page.CurriculumComponent)
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];

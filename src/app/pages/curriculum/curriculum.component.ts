d05906046900dfe8d0ed2d659106dca031e80a04
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-04-26 23:00:29 +0200

    main structure ready

--- src/app/pages/curriculum/curriculum.component.ts
+++ src/app/pages/curriculum/curriculum.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 
 import { map, tap } from 'rxjs/operators';
 
@@ -9,21 +9,28 @@ import { CustomTranslateService } from '../../services/custom-translate.service'
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { MainComponent } from '../../shared/main/main.component';
+import { IdentificationService } from '../../services/identification.service';
 
 @Component({
   imports: [AsideComponent, FooterComponent, HeaderComponent, MainComponent],
   templateUrl: './curriculum.component.html',
-  styleUrl: './curriculum.component.css',
+  styleUrl: './curriculum.component.scss',
 })
 export class CurriculumComponent {
   private readonly activatedRoute = inject(ActivatedRoute);
   private readonly customTranslateService = inject(CustomTranslateService);
+  private readonly identificationService = inject(IdentificationService);
 
   private readonly activatedRoute$ = this.activatedRoute.paramMap.pipe(
     map((paramMap) => paramMap.get('lang') ?? ''),
     tap((lang) => this.customTranslateService.chageLanguage(lang))
   );
-  currentLang = toSignal(this.activatedRoute$, {
+  private readonly currentLang = toSignal(this.activatedRoute$, {
     initialValue: this.customTranslateService.currentLang(),
+  });
+
+  identificationResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) => this.identificationService.getIdentification(),
   });
 }

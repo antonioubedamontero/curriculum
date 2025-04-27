845f26fe9d2fcfe11282e0f951f8d6e30e2a8672
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-04-27 08:44:38 +0200

    aside ready

--- src/app/shared/aside/aside.component.ts
+++ src/app/shared/aside/aside.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+
+import { IdentificationResponse } from '../../interfaces';
 
 @Component({
   selector: 'app-aside',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './aside.component.html',
-  styleUrl: './aside.component.scss'
+  styleUrl: './aside.component.scss',
 })
 export class AsideComponent {
-
+  identificationResponse = input.required<IdentificationResponse>();
 }

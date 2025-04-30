33137409de80de020660d123f3cabd796100e9a8
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-04-30 15:46:47 +0200

    habilities ready

--- src/app/shared/main/components/habilities/habilities.component.ts
+++ src/app/shared/main/components/habilities/habilities.component.ts
@@ -1,6 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
+
+import { MainService } from '../../../../services/main.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'main-habilities',
@@ -9,4 +17,12 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './habilities.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HabilitiesComponent {}
+export class HabilitiesComponent {
+  mainService = inject(MainService);
+
+  habilitiesResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) => this.mainService.getHabilities(),
+  });
+  habilities = computed(() => this.habilitiesResource.value()?.habilities);
+}
